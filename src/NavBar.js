e6f7ad533307db80ae04114fe51e9d0718077e64
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-09 16:56:29 -0400

    styles the navbar as the #App-header

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,10 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className="navbar">
-      <NavLink to="/signup">Sign Up</NavLink><br/>
-      <NavLink to="/vr">VR</NavLink><br/>
+    <div className="navbar" id="App-header">
+      <img src="tarot.png" alt="TarotVR Logo" width="46px" height="46px"/>
       <NavLink to="/">Home</NavLink>
+      <NavLink to="/signup">Sign Up</NavLink>
+      <NavLink to="/vr">VR</NavLink>
 
     </div>
   );
