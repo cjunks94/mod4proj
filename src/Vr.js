7ca189abcf7ec62bf58b379e02650c3363c93640
cjunks94 <cjunks94@gmail.com>
2018-07-09 19:26:50 -0400

    green is card deck. on click remove deck and render card

--- src/Vr.js
+++ src/Vr.js
@@ -4,36 +4,58 @@ import 'aframe'
 import 'aframe-extras'
 
 
-const Vr = (props) => {
+class Vr extends React.Component{
+createCard = () =>{
+  const scene = document.getElementsByTagName('a-scene')[0]
 
+  const newBox ='<a-box id="new-card"position="0 1.8 -3" rotation="0 0 0" material=" color: aqua" depth=".001" height="3" width="2"></a-box>'
+  scene.insertAdjacentHTML( 'beforeend', newBox)
+  scene.removeChild(document.getElementById('card-deck'))
+
+
+}
+
+removeDeck = () =>{
+  document.getElementsByTagName('a-scene')[0]
+  .removeChild(document.getElementById('card-deck'))
+}
+
+
+render(){
   return (
+  <div>
+
+
     <a-scene new-scene>
       <a-camera>
         <a-cursor></a-cursor>
-      </a-camera>
-              <a-box id="a-box" position="-2 3 -3" rotation="0 0 0" material="src: exit.png" depth=".001" height=".5" width=".75" onClick={props.test}></a-box>
+    </a-camera>
+
+    <a-box position="-2 3 -3" rotation="0 0 0" material="src: exit.png" depth=".001" height=".5" width=".75" onClick={this.props.test}></a-box>
+
+  <a-box id="card-deck" position="0 1.8 -3" color="green" onClick={this.createCard}></a-box>
+  {/* {this.createBox()} */}
 
 {/* box material will be changed to "src: 'our tarot img url'" */}
-      <a-box id="a-box" position="0 1.8 -3" rotation="0 0 0" material="color: aqua" depth=".001" height="3" width="2" onClick={props.test}></a-box>
+
+      <a-entity light="type: point; intensity: 5" position="0 0 0"></a-entity>
 
 
-
-
-
-      {/* <a-entity position="0 2 -3" rotation="0 0 0" geometry="primitive: box" material="src: #my-texture"></a-entity> */}
-
+      <a-entity id="rain" particle-system="preset: rain; color: #24CAFF; particleCount: 5000"></a-entity>
 
     <a-sky material="src: space.png"></a-sky>
     <a-ocean
-      color="black"
-      depth="100"
-      width="100"
-      opacity="1"
+      color="purple"
+      depth="50"
+      width="50"
+      density="20"
+      opacity=".9"
       amplitude=".5"
       sound="src: url(sea.mp3); autoplay: true; loop: true; volume: .75"></a-ocean>
 
     </a-scene>
-  );
+    </div>
+  )}
 };
 
 export default Vr;
