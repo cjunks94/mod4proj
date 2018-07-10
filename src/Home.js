f529ec9a2a64f3b8422d93ca161217f12d5e662f
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-10 16:59:31 -0400

    redesigns home page to contain information about each Tarot card and instructions on using the app

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import TarotImg from './components/TarotImg'
+import Card from './components/Card'
 
-const Home = () => {
+const Home = (props) => {
+  console.log(props)
   return (
     <div>
-      <h1>Tarot Card Home Page</h1>
+      <h2>Welcome to TarotVR!</h2>
+      <p>Select one of the following options:</p>
+      <ul>
+        <li>VR: Experience our 3D immersive Tarot Card Reading for yourself</li>
+        <li>Sign Up: Create a profile to save your Tarot Card Readings</li>
+        <li>Login: Access your profile page to view your previous readings</li>
+      </ul>
+      <h4>Select A Card For More Information</h4>
+      <div className="scrolling-wrapper">
+        {props.cards.length > 0 ? props.cards.map( card => <TarotImg key={card.id} card={card} handleClick={props.handleImgClick}/>) : null }
+      </div>
+      <div className="show-card">
+        {props.currentCard.id ? < Card card={props.currentCard} /> : null }
+      </div>
     </div>
   );
 };
