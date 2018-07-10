f0f1398711623e0b089550bc3d0977c801c8ed2b
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-10 16:58:34 -0400

    adds functionality for fetching cards and user authentication and passing to its children

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import {
   Route
 } from 'react-router-dom';
 import Navbar from './NavBar';
-import SignUp from './SignUp';
+import SignUpContainer from './SignUpContainer';
 import Home from './Home';
 import Vr from './Vr';
+import SignUpContainer from './SignUpContainer';
 
 
 
@@ -16,18 +17,79 @@ class App extends React.Component{
     super()
     // set state with stuff i thought was useful
     this.state ={
-      currentCard:'',
-      currentser:'',
-      allCards:[],
+      auth: {
+        currentUser: {}
+      },
+      currentCard: {},
+      cards:[]
     }
   }
+
+  componentDidMount () {
+    const token = localStorage.getItem('token')
+    if (token) {
+      this.getAuth(token)
+    }
+
+    this.getCards()
+  }
+
+  getCards = () => {
+    fetch('http://localhost:3000/api/v1/cards')
+    .then(resp => resp.json())
+    .then(cards => this.setState({ cards }))
+  }
+
+  getAuth = (token) => {
+    const options =   {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': token
+      }
+    }
+    fetch('http://localhost:3000/api/v1/reauth', options)
+    .then(resp => resp.json())
+    .then(user => {
+      this.setState({
+        auth: {
+          currentUser: user
+        }
+      })
+    })
+  }
+
+  handleLogin = (user) => {
+    this.setState({
+        auth: {
+          currentUser: user
+        }
+      }, () => {
+        localStorage.setItem('token', user.id)
+      })
+  }
+
+  handleLogout = () => {
+    this.setState({
+      auth: {
+        currentUser: {}
+      }
+    })
+    localStorage.clear()
+  }
+
+  handleImgClick = (e) => {
+    const currentCard = this.state.cards.find( card => card.id == e.target.id)
+    this.setState({ currentCard })
+  }
+
   render(){
   return (
     <Router>
       <div>
-        <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signup" component={SignUp} />
+        <Navbar currentUser={this.state.auth.currentUser} handleLogin={this.handleLogin} handleLogout={this.handleLogout}/>
+        <Route exact path="/" render={(props) => <Home {...props} cards={this.state.cards} currentCard={this.state.currentCard} handleImgClick={this.handleImgClick} />} />
+        <Route exact path="/signup" component={SignUpContainer} />
         <Route exact path="/vr" component={Vr} />
       </div>
     </Router>
