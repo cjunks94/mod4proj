96bf344ac1a2b7362d682c9afce9b02b1fb1fe34
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-10 16:55:25 -0400

    creates a component to render signup form

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SignUpForm = (props) => {
+    return (
+        <form onChange={props.handleChange} onSubmit={props.handleSubmit}>
+            <input type='text' name="username" placeholder='Username'/><br/>
+            <input type='password' name="password" placeholder='Password' /><br/>
+            <input type='password' name="passwordConfirmation" placeholder='Confirm Password' /><br/>
+            <button type='submit' >Submit</button>
+            {/* on submit send login back to app for state/local storage changeups */}
+      </form>
+    )
+};
+
+export default SignUpForm;
