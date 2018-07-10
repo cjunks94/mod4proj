c7ac49f8f3d0c01b3139b7859a8870166dd1b876
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-10 17:00:41 -0400

    adds Login Form to nav bar, functionality for when the user is logged in, and classes and elements for styling

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => {
-  return (
-    <div className="navbar" id="App-header">
-      <img src="tarot.png" alt="TarotVR Logo" width="46px" height="46px"/>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/signup">Sign Up</NavLink>
-      <NavLink to="/vr">VR</NavLink>
+import LoginForm from './components/LoginForm';
 
+const NavBar = (props) => {
+  const loggedIn = props.currentUser.id
+  
+  return (
+    <div className="navbar top fixed" id="App-header">
+      <div className="navbar left">
+        <img src="tarot.png" alt="TarotVR Logo" width="46px" height="46px"/>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/vr" target="_blank">VR</NavLink>
+        {loggedIn ? null : <NavLink to="/signup">Sign Up</NavLink> }      
+      </div>
+      <div className="navbar right">
+        {loggedIn ? <button className="logout" onClick={props.handleLogout}> Log Out </button> : (
+              < LoginForm handleLogin={props.handleLogin} />
+            )}
+      </div>
     </div>
   );
 };
