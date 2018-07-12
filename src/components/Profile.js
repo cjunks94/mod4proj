c703268f3428a26b5673fbbdbbe648cb01f18c24
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-12 15:09:32 -0400

    renders profile information

---
+++ src/components/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = (props) => {
+    const {username, email, first_name, last_name, readings} = props.user
+    const name = first_name && last_name ? first_name + ' ' + last_name : first_name ? first_name : username
+
+    return (
+        <div className="profile">
+            <h2>{name}</h2>
+            { email ? <p><strong>Email:</strong> {email}</p> : null}
+            <button className="button" id="edit" onClick={props.handleEditClick}>Edit</button>
+            {/* render readings */}
+        </div>
+    )
+};
+
+export default Profile;
