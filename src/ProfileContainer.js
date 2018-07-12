ae2ac0492ad37b8e956e4211c0b5654fbceb53f3
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-12 15:06:33 -0400

    changes container to render Profile component and add more functionality

--- src/ProfileContainer.js
+++ src/ProfileContainer.js
@@ -1,33 +1,70 @@
 import React from 'react';
+import Profile from './components/Profile';
+import Form from './components/Form';
 
 class ProfileContainer extends React.Component {
     constructor(props) {
         super(props)
         console.log(props);
         this.state = {
-            fields: props.user,
-            onEdit: false
+            onEdit: false,
+            fields: {
+                username: props.user.username,
+                firstName: props.user.first_name,
+                lastName: props.user.last_name,
+                email: props.user.email
+            },
+            data: {}
         }
     }
 
-    handleEditClick = () => {
-        console.log('clicked');
+    updateUser = () => {
+        const options = {
+            method: 'PATCH',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            },
+            body: JSON.stringify(this.state.data)
+        }
+        fetch(`http://localhost:3000/api/v1/users/${this.props.user.id}`, options)
+        .then(resp => resp.json())
+        .then(user => this.setState({
+            user
+        }))
     }
 
-    handleEditSubmit = () => {
+    handleEditClick = () => {
+        this.setState({
+            onEdit: true
+        })
+    }
+
+    handleEditChange = (e) => {
+        const name = e.target.name.replace(/_([a-z])/g, (str) => str[1].toUpperCase() )
+        const newFields = { ...this.state.fields, [name]: e.target.value };
+
+        this.setState({ 
+          fields: newFields, 
+          data: {...this.state.data, [e.target.name]: e.target.value}
+        });
+    }
+
+    handleEditSubmit = (e) => {
+        e.preventDefault;
         console.log('submitted');
+        this.updateUser();
+        this.setState({
+            onEdit: false
+        });
     }
 
     render() {
-        const {username, email, first_name, last_name, readings} = this.state.fields
-        const name = first_name && last_name ? first_name + ' ' + last_name : first_name ? first_name : username
-        
         return (
             <div className="profile page-body">
-                <h2>{name}</h2>
-                { email && <p><strong>Email:</strong> {email}</p> }
-                <button className="button" id="edit" onClick={this.handleEditClick}>Edit</button>
-                {/* render readings */}
+                { this.state.onEdit ? 
+                    <Form fields={this.state.fields} handleSubmit={this.handleEditSubmit} handleChange={this.handleEditChange}/> : 
+                    <Profile user={this.props.user} handleEditClick={this.handleEditClick} />}
                 {/* renders edit form */}
             </div>
         )
