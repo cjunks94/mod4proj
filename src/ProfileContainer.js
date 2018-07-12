58de1bf6b11264e6a1aa805ef59ec32b6b2e3b9e
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-12 10:12:37 -0400

    creates a profile page container for storing states and functionality

--- src/ProfileContainer.js
+++ src/ProfileContainer.js
@@ -3,17 +3,34 @@ import React from 'react';
 class ProfileContainer extends React.Component {
     constructor(props) {
         super(props)
-
+        console.log(props);
         this.state = {
-            username: '',
-            password: ''
+            fields: props.user,
+            onEdit: false
         }
     }
 
+    handleEditClick = () => {
+        console.log('clicked');
+    }
+
+    handleEditSubmit = () => {
+        console.log('submitted');
+    }
+
     render() {
-        return (<div className="profile">
-            Profile Page
-        </div>)
+        const {username, email, first_name, last_name, readings} = this.state.fields
+        const name = first_name && last_name ? first_name + ' ' + last_name : first_name ? first_name : username
+        
+        return (
+            <div className="profile page-body">
+                <h2>{name}</h2>
+                { email && <p><strong>Email:</strong> {email}</p> }
+                <button className="button" id="edit" onClick={this.handleEditClick}>Edit</button>
+                {/* render readings */}
+                {/* renders edit form */}
+            </div>
+        )
     }
 };
 
