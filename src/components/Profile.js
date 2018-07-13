9520dde98b71e181b9045d660a5b89e98c2c09af
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-13 15:38:45 -0400

    profile page now renders readings

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Reading from './Reading';
+
 const Profile = (props) => {
     const {username, email, first_name, last_name, readings} = props.user
     const name = first_name && last_name ? first_name + ' ' + last_name : first_name ? first_name : username
@@ -8,8 +10,10 @@ const Profile = (props) => {
         <div className="profile">
             <h2>{name}</h2>
             { email ? <p><strong>Email:</strong> {email}</p> : null}
+            <p><strong>Readings:</strong> {readings.length}</p>
             <button className="button" id="edit" onClick={props.handleEditClick}>Edit</button>
-            { console.log(props.user.readings)}
+            <h4><u>Your Recent Readings</u></h4> 
+            { readings.map(reading => <Reading reading={reading} />).reverse()}
         </div>
     )
 };
