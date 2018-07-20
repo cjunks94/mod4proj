467c2746a7116ccc1ca848be24b06454849a0184
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-07-20 10:35:13 -0400

    changes test URL to include proper navigation

--- src/baseURL.js
+++ src/baseURL.js
@@ -1,3 +1,3 @@
-const BASEURL='https://tarot-vr-api.herokuapp.com/api/v1'
-// const BASEURL='https://localhost:3000'
+// const BASEURL='https://tarot-vr-api.herokuapp.com/api/v1'
+const BASEURL='http://localhost:3001/api/v1'
 export default BASEURL;
